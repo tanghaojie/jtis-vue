10c1b05c23b269dc42780526ce79c2c289a48f66
tanghaojie <thj3360016@gmail.com>
2025-01-10 15:12:39 +0800

    feat:

--- packages/composables/src/dom/useResizeDetector.ts
+++ packages/composables/src/dom/useResizeDetector.ts
@@ -1,8 +1,8 @@
 import { ref, onMounted, onBeforeUnmount } from 'vue'
 import { addListener, removeListener, ResizeCallback } from 'resize-detector'
 
-export function useResizeDetector(cb: ResizeCallback<HTMLElement>) {
-  const el = ref<HTMLElement | null>()
+export function useResizeDetector<E extends HTMLElement = HTMLElement>(cb: ResizeCallback<E>) {
+  const el = ref<E | null>()
 
   onMounted(function () {
     el.value && addListener(el.value, cb)
